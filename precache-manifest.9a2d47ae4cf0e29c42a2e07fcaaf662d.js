3a148b1d855143373f03cbb024c5dd093540c222
Alquinn John Gayatao <alquinngayatao@gmail.com>
2018-09-02 01:42:54 +0800

    vue project commit

---
+++ precache-manifest.9a2d47ae4cf0e29c42a2e07fcaaf662d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ebe10564e00abf8d91d0",
+    "url": "/js/chunk-vendors.4f4f2361.js"
+  },
+  {
+    "revision": "2f183e1b82a69185bb71",
+    "url": "/js/app.58aa9aa2.js"
+  },
+  {
+    "revision": "7e2f8c4762090d2f0b72",
+    "url": "/js/about.2d341050.js"
+  },
+  {
+    "revision": "133f022f1ef871ce847ec3626377276c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2f183e1b82a69185bb71",
+    "url": "/css/app.6100e5bc.css"
+  }
+];
